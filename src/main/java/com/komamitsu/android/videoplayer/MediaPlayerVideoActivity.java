1436a1aa6b586a8b6abaaaf3edd0c3d4f16877e9
Mitsunori Komatsu <komamitsu@gmail.com>
2012-05-28 00:37:43 +0900

    initial commit

---
+++ src/main/java/com/komamitsu/android/videoplayer/MediaPlayerVideoActivity.java
@@ -0,0 +1,170 @@
+package com.komamitsu.android.videoplayer;
+
+import android.app.Activity;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
+import android.media.MediaPlayer.OnBufferingUpdateListener;
+import android.media.MediaPlayer.OnCompletionListener;
+import android.media.MediaPlayer.OnPreparedListener;
+import android.media.MediaPlayer.OnVideoSizeChangedListener;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.widget.Toast;
+
+
+public class MediaPlayerVideoActivity extends Activity implements
+        OnBufferingUpdateListener, OnCompletionListener,
+        OnPreparedListener, OnVideoSizeChangedListener, SurfaceHolder.Callback {
+
+    private static final String TAG = MediaPlayerVideoActivity.class.getSimpleName();
+    private int mVideoWidth;
+    private int mVideoHeight;
+    private MediaPlayer mMediaPlayer;
+    private SurfaceView mPreview;
+    private SurfaceHolder holder;
+    private String path;
+    private Bundle extras;
+    private static final String MEDIA = "media";
+    private boolean mIsVideoSizeKnown = false;
+    private boolean mIsVideoReadyToBePlayed = false;
+
+    /**
+     * 
+     * Called when the activity is first created.
+     */
+    @Override
+    public void onCreate(Bundle icicle) {
+        super.onCreate(icicle);
+        setContentView(R.layout.player);
+        mPreview = (SurfaceView) findViewById(R.id.surface);
+        path = getIntent().getStringExtra("url");
+        holder = mPreview.getHolder();
+        holder.addCallback(this);
+        holder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
+        extras = getIntent().getExtras();
+
+    }
+
+    private void playVideo(Integer Media) {
+        doCleanUp();
+        try {
+
+            /*
+             * TODO: Set path variable to progressive streamable mp4 or
+             * 3gpp format URL. Http protocol should be used.
+             * Mediaplayer can only play "progressive streamable
+             * contents" which basically means: 1. the movie atom has to
+             * precede all the media data atoms. 2. The clip has to be
+             * reasonably interleaved.
+             * 
+             */
+            if (path == "") {
+                // Tell the user to provide a media file URL.
+                Toast
+                        .makeText(
+                                MediaPlayerVideoActivity.this,
+                                "Please edit MediaPlayerDemo_Video Activity,"
+                                        + " and set the path variable to your media file URL.",
+                                Toast.LENGTH_LONG).show();
+
+            }
+
+            // Create a new media player and set the listeners
+            mMediaPlayer = new MediaPlayer();
+            mMediaPlayer.setDataSource(path);
+            mMediaPlayer.setDisplay(holder);
+            mMediaPlayer.prepare();
+            mMediaPlayer.setOnBufferingUpdateListener(this);
+            mMediaPlayer.setOnCompletionListener(this);
+            mMediaPlayer.setOnPreparedListener(this);
+            mMediaPlayer.setOnVideoSizeChangedListener(this);
+            mMediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
+
+
+        } catch (Exception e) {
+            Log.e(TAG, "error: " + e.getMessage(), e);
+        }
+    }
+
+    public void onBufferingUpdate(MediaPlayer arg0, int percent) {
+        Log.d(TAG, "onBufferingUpdate percent:" + percent);
+
+    }
+
+    public void onCompletion(MediaPlayer arg0) {
+        Log.d(TAG, "onCompletion called");
+    }
+
+    public void onVideoSizeChanged(MediaPlayer mp, int width, int height) {
+        Log.v(TAG, "onVideoSizeChanged called");
+        if (width == 0 || height == 0) {
+            Log.e(TAG, "invalid video width(" + width + ") or height(" + height + ")");
+            return;
+        }
+        mIsVideoSizeKnown = true;
+        mVideoWidth = width;
+        mVideoHeight = height;
+        if (mIsVideoReadyToBePlayed && mIsVideoSizeKnown) {
+            startVideoPlayback();
+        }
+    }
+
+    public void onPrepared(MediaPlayer mediaplayer) {
+        Log.d(TAG, "onPrepared called");
+        mIsVideoReadyToBePlayed = true;
+        if (mIsVideoReadyToBePlayed && mIsVideoSizeKnown) {
+            startVideoPlayback();
+        }
+    }
+
+    public void surfaceChanged(SurfaceHolder surfaceholder, int i, int j, int k) {
+        Log.d(TAG, "surfaceChanged called");
+
+    }
+
+    public void surfaceDestroyed(SurfaceHolder surfaceholder) {
+        Log.d(TAG, "surfaceDestroyed called");
+    }
+
+
+    public void surfaceCreated(SurfaceHolder holder) {
+        Log.d(TAG, "surfaceCreated called");
+        playVideo(extras.getInt(MEDIA));
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        releaseMediaPlayer();
+        doCleanUp();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        releaseMediaPlayer();
+        doCleanUp();
+    }
+
+    private void releaseMediaPlayer() {
+        if (mMediaPlayer != null) {
+            mMediaPlayer.release();
+            mMediaPlayer = null;
+        }
+    }
+
+    private void doCleanUp() {
+        mVideoWidth = 0;
+        mVideoHeight = 0;
+        mIsVideoReadyToBePlayed = false;
+        mIsVideoSizeKnown = false;
+    }
+
+    private void startVideoPlayback() {
+        Log.v(TAG, "startVideoPlayback");
+        holder.setFixedSize(mVideoWidth, mVideoHeight);
+        mMediaPlayer.start();
+    }
+}
---
+++ src/main/java/com/komamitsu/android/videoplayer/UrlInputActivity.java
@@ -0,0 +1,34 @@
+package com.komamitsu.android.videoplayer;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+
+
+public class UrlInputActivity extends Activity {
+    private static final String TAG = UrlInputActivity.class.getSimpleName();
+
+    @Override
+    public void onCreate(Bundle icicle) {
+        super.onCreate(icicle);
+        setContentView(R.layout.url_input);
+        final EditText url = (EditText) findViewById(R.id.url);
+        Button submitBtn = (Button) findViewById(R.id.submit);
+        submitBtn.setOnClickListener(new OnClickListener() {
+          @Override
+          public void onClick(View v) {
+            String urlText = url.getText().toString();
+            if (urlText.length() > 0) {
+              Intent intent = new Intent(UrlInputActivity.this, MediaPlayerVideoActivity.class);
+              intent.putExtra("url", url.getText().toString());
+              intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+              startActivity(intent);
+            }
+          }
+        });
+    }
+}
